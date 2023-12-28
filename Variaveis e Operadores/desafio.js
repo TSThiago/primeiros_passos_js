92eb33bb1c0405e0d19dc5b396adc9983f506415
Thiago Shibanuma <thiago.tshibanuma@gmail.com>
2023-12-27 23:56:03 -0300

    feat: challenge 1 done

---
+++ Variaveis e Operadores/desafio.js
@@ -0,0 +1,6 @@
+let precoCombustivel = parseFloat(5.79);
+let kmPorLitro = parseFloat(12);
+let distancia = parseFloat(120);
+
+let gastoTotal = (distancia/kmPorLitro) * precoCombustivel;
+console.log(gastoTotal);
